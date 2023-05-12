7b57ede3ec30525338bd0b9898ca8d59ab318819
Ibrahimsyah Zairussalam <ibrahimsyah.zairussalam@tokopedia.com>
2023-05-12 21:38:54 +0700

    feat(sidenav): menu logic improvement

--- src/components/layout/sidebar/index.jsx
+++ src/components/layout/sidebar/index.jsx
@@ -3,22 +3,25 @@ import {
 } from 'antd'
 import {
     useLocation,
-    useNavigate
+    useNavigate,
+    useParams
 } from 'react-router-dom'
 import NavMenu from '@/routes/navmenu'
 
 const { Sider } = Layout
 const Sidebar = () => {
     const { pathname } = useLocation()
+    const { id } = useParams()
     const navigate = useNavigate()
-    
+
+    const menu = NavMenu.get({ appID: id })
     return <Sider
         className='sider'
         breakpoint='lg'
         collapsible
     >
         <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-        <Menu theme='dark' selectedKeys={[pathname]} items={NavMenu[pathname.split('/')[1]]} onClick={({key}) => navigate(key)} />
+        <Menu theme='dark' selectedKeys={[pathname]} items={menu[pathname.split('/')[1]]} onClick={({ key }) => navigate(key)} />
     </Sider>
 }
 
--- src/routes/navmenu.jsx
+++ src/routes/navmenu.jsx
@@ -1,26 +1,50 @@
-import {HomeOutlined, HistoryOutlined} from '@ant-design/icons'
+import { HomeOutlined, HistoryOutlined, ToolOutlined, DollarOutlined } from '@ant-design/icons'
 
 export default {
-    '': [
-        {
-            icon: <HomeOutlined />,
-            label: "Apps",
-            key: "/"
-        }, {
-            icon: <HistoryOutlined />,
-            label: "Build History",
-            key: "/history"
-        }
-    ],
-    apps: [
-        {
-            icon: <HomeOutlined />,
-            label: "Apps",
-            key: "/"
-        }, {
-            icon: <HistoryOutlined />,
-            label: "Content",
-            key: "#content"
-        }
-    ]
+    get: ({ appID }) => ({
+        '': [
+            {
+                icon: <HomeOutlined />,
+                label: "Apps",
+                key: "/"
+            }, {
+                icon: <HistoryOutlined />,
+                label: "Build History",
+                key: "/history"
+            }
+        ],
+        history: [
+            {
+                icon: <HomeOutlined />,
+                label: "Apps",
+                key: "/"
+            }, {
+                icon: <HistoryOutlined />,
+                label: "Build History",
+                key: "/history"
+            }
+        ],
+        apps: [
+            {
+                icon: <HomeOutlined />,
+                label: "Apps",
+                key: "/"
+            },
+            {
+                icon: <HistoryOutlined />,
+                label: "Content",
+                key: `/apps/${appID}/content`
+            },
+            {
+                icon: <DollarOutlined />,
+                label: "Monetization",
+                key: `/apps/${appID}/monetization`
+            },
+            {
+                icon: <ToolOutlined />,
+                label: "Build",
+                key: `/apps/${appID}/build`
+            }
+        ]
+    })
 }
